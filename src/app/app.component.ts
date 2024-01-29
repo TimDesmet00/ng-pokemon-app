611e80fa42647490e3b77b7e27ddf091f75f6a9e
tim <timdesmet00@gmail.com>
2024-01-29 14:15:26 +0100

    chapitre composant du tuto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list'
+import { Pokemon } from './pokemon'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: `<h1>Welcome to {{ pokemonList[0] }}!</h1>`,
+  template: `<h1>Liste de Pokémons</h1>`,
 })
 export class AppComponent implements OnInit {
-  pokemonList = ['Bulbizarre', 'Salamèche', 'Carapuce'];
+  pokemonList: Pokemon[] = POKEMONS;
 
   ngOnInit() {
     console.table(this.pokemonList)
+    this.selectPokemon(this.pokemonList[0])
+  }
+
+  selectPokemon(Pokemon: Pokemon) {
+    console.log(`Vous avez cliqué sur le pokémon ${Pokemon.name} !`);
   }
 }
