5242bc0ae5af44afed74e9f1d50f551345698bac
tim <timdesmet00@gmail.com>
2024-01-30 12:16:19 +0100

    fin chapitre pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,15 @@ import { POKEMONS } from './mock-pokemon-list';
 import { Pokemon } from './pokemon';
 import { CommonModule } from '@angular/common';
 import { BorderCardDirective } from './border-card.directive';
+import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: 'app.component.html',
-  imports: [CommonModule, BorderCardDirective],
+  imports: [CommonModule, BorderCardDirective, PokemonTypeColorPipe],
   styleUrls: ['app.component.scss'],
+  
 })
 export class AppComponent implements OnInit {
   pokemonList: Pokemon[] = POKEMONS;
