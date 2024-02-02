c594afc1d25cda3a8a2312ce0833b9c271fd3b34
tim <timdesmet00@gmail.com>
2024-02-02 10:47:43 +0100

    debut requete http bloquer par les module utiliser dans le tuto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
 import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { PokemonFormComponent } from './pokemon-form/pokemon-form.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AppModule } from './app.module';
+
+
 
 @Component({
   selector: 'app-root',
@@ -11,6 +15,8 @@ import { PokemonFormComponent } from './pokemon-form/pokemon-form.component';
   templateUrl: 'app.component.html',
   imports: [
     FormsModule,
+    HttpClientModule,
+    AppModule,
     ListPokemonComponent,
     DetailPokemonComponent,
     PokemonFormComponent,
---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api'
+import { POKEMONS } from './mock-pokemon-list';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService implements InMemoryDbService {
+
+  createDb() {
+    return { POKEMONS };
+
+
+  }
+}
