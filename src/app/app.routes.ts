298baf52377a3b389eb57b7b85c2b5464bed9818
tim <timdesmet00@gmail.com>
2024-02-05 08:33:38 +0100

    verification

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,21 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { EditPokemonComponent } from './edit-pokemon/edit-pokemon.component';
 
 export const routes: Routes = [
-    { path : 'edit/pokemon/:id', component: EditPokemonComponent },
-    { path : 'pokemons', component: ListPokemonComponent },
-    { path : 'pokemon/:id', component: DetailPokemonComponent },
-    { path : '', redirectTo: 'pokemons', pathMatch: 'full' },
-    { path : '**', component: PageNotFoundComponent }
+    { path : 
+       'edit/pokemon/:id', loadComponent: () => 
+        import('./edit-pokemon/edit-pokemon.component').then(module => module.EditPokemonComponent) 
+    },
+    { path :
+        'pokemons', loadComponent: () =>
+        import('./list-pokemon/list-pokemon.component').then(module => module.ListPokemonComponent)
+     },
+    { path :
+        'pokemon/:id', loadComponent: () =>
+        import('./detail-pokemon/detail-pokemon.component').then(module => module.DetailPokemonComponent)
+    },
+    { path :'', redirectTo: 'pokemons', pathMatch: 'full' },
+    { path :
+        '**', loadComponent: () => 
+        import('./page-not-found/page-not-found.component').then(module => module.PageNotFoundComponent)
+    }
 ];
